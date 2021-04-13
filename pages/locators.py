017723647185412474d31e8ca3e0c961e702fff3
Iuliia Sharina <yulia.hydrologist@yandex.ru>
2021-04-13 17:40:29 +0300

    Добавлены локаторы для страницы заказа

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,14 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTRATION_FORM = (By.CSS_SELECTOR, ".register_form")
+
+class ProductPageLocators():
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.TAG_NAME, "h1")
+    PRICE = (By.CSS_SELECTOR, ".price_color")
+    PRODUCT_IN_BASKET = (By.XPATH, "/html/body/div/div/div/div/div/strong")
+    PRICE_IN_BASKET = (By.XPATH, "/html/body/div/div/div/div/div/p/strong")
+    SUCCESS_MESSAGE = (By.XPATH, "/html/body/div/div/div/div/div")
+
+    
+   
