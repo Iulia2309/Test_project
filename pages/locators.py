7f0f6460ad4b42d43c2d35ea214af4ee42d9b9fe
Iuliia Sharina <yulia.hydrologist@yandex.ru>
2021-04-16 15:26:31 +0300

    Добавлен локатор кнопки перехода в корзину

--- pages/locators.py
+++ pages/locators.py
@@ -3,10 +3,12 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    BASKET_BUTTON = (By.PARTIAL_LINK_TEXT, "basket")
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTRATION_FORM = (By.CSS_SELECTOR, ".register_form")
+
 
 class ProductPageLocators():
     BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
@@ -19,6 +21,12 @@ class ProductPageLocators():
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+  
 
-    
-   
+class BasketPageLocators():
+    #EMPTY_BASKET = (By.CSS_SELECTOR, "#content_inner>p")
+    #EMPTY_BASKET = (By.XPATH, "//div[#content_inner]/p[contains(text(), 'Your basket is empty')]")
+    BASKET_FORMSET = (By.CSS_SELECTOR, "#basket_formset")
+    EMPTY_BASKET = (By.XPATH, "//div/form[@id ='register_form']/div/button")
+
+
