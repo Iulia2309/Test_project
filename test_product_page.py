78f6a6b5d2e8412645dbccffc5e80351046a8524
Iuliia Sharina <yulia.hydrologist@yandex.ru>
2021-04-16 15:08:33 +0300

    Добавлен тест на отсутствие видимости продукта в корзине

--- test_product_page.py
+++ test_product_page.py
@@ -2,6 +2,7 @@ import pytest
 from .pages.product_page import ProductPage
 from .pages.base_page import BasePage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -58,8 +59,15 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()                    
     
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/"
+    page = BasePage(browser, link)                        
+    page.open()
+    page.go_to_basket()
+    page = BasketPage(browser, browser.current_url)
+    page.should_be_empty_basket()
 
 
-
+    
 
     
