04df062bc7b8c97d56b33efc5639541df6aef499
Iuliia Sharina <yulia.hydrologist@yandex.ru>
2021-04-20 15:19:43 +0300

    Добавлена проверка регистрации пользователя

--- pages/base_page.py
+++ pages/base_page.py
@@ -66,7 +66,9 @@ class BasePage():
         basket_button = self.browser.find_element(*MainPageLocators.BASKET_BUTTON)
         basket_button.click()
 
-
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                     " probably unauthorised user"
 
         
       
