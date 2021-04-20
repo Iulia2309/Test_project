28acec8fc8aa47ac0a1e0fb8367b2a740b42fdc6
Iuliia Sharina <yulia.hydrologist@yandex.ru>
2021-04-20 15:21:01 +0300

    Добавлены локаторы для регистрации

--- pages/locators.py
+++ pages/locators.py
@@ -8,7 +8,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTRATION_FORM = (By.CSS_SELECTOR, ".register_form")
-
+    REGISTRATION_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTRATION_PASSWORD_1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTRATION_PASSWORD_2 = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTRATION_BUTTON = (By.XPATH, "//form[@id ='register_form']/button")
 
 class ProductPageLocators():
     BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
@@ -21,7 +24,7 @@ class ProductPageLocators():
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
-  
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 class BasketPageLocators():
     #EMPTY_BASKET = (By.CSS_SELECTOR, "#content_inner>p")
