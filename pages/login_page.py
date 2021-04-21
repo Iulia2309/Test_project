c172931185af3113189691c35258c5ab36f7257f
Iuliia Sharina <yulia.hydrologist@yandex.ru>
2021-04-21 17:17:21 +0300

    уьраны лишние стороки

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,5 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
-import time
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -9,15 +8,12 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
         assert self.browser.current_url.find('login') != -1, "login is not in current link"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Registration form is not presented"
 
     def register_new_user(self, email, password):
